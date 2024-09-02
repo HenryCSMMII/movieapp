87f5eed41184ea08a3dff68739bb5c3577a71df6
HenryCS <henryca8@hotmail.com>
2024-09-02 17:14:42 -0500

    add section about

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,21 @@
 export default function About() {
     return (
-      <div>
-        About
+      <div className="max-w-6xl mx-auto p-3 gap-4 space-y-4">
+        <h1 className="text-2xl font-medium text-amber-600">About</h1>
+        <p>
+            Welcome to our movie database website! We area a team of passionate movie
+            enthusiasts who have come together to create a one-stop destination for
+            all your movie-related needs.
+        </p>
+
+        <p>
+            Our website is designed to provide you with a comprehensive database of
+            movies from all around the world, along with  the latest news, reviews,
+            and trailers. Our movie database is constantly updated with new releases, 
+            ensuring that you have access to the latest and greatest movies. You can
+            search for movies by title, director, actor, or release date, making
+            it easy to find the perfect movie for any occasion.
+        </p>
       </div>
     )
 }
